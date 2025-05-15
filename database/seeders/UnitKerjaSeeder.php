be8a1fbe8ee3c7b7bcbd994805807ff8d5b4e8ce
Aprilia1104 <salsabilapril85@gmail.com>
2025-05-15 15:18:42 +0700

    upload praktikum

---
+++ database/seeders/UnitKerjaSeeder.php
@@ -0,0 +1,20 @@
+<?php
+namespace Database\Seeders;
+
+use App\Models\Unitkerja;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UnitKerjaSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        UnitKerja::create([
+            "kode" => "A123",
+            "nama" => "LPPM"
+        ]);
+    }
+}
